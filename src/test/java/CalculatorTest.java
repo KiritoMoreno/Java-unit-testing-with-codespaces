abdcf88276747cf924e9843b3dd3963865fd9ba7
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-12-03 15:55:22 +0100

    Basic Junit-test.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,10 +6,34 @@ public class CalculatorTest {
 
   @Test
   public void testSum() {
-    int expected = 3; // Corregido de 'inte' a 'int'
+    int expected = 3;
 
     int actual = calculator.sum(1, 2);
 
     assertEquals(expected, actual);
   }
+
+  @Test
+  public void testSubtract() {
+    int expected = 0;
+
+    int actual = calculator.subtract(3, 3);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testMultiply() {
+    int expected = 10;
+    int actual = calculator.multiply(2, 5);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testDivide() {
+    int expected = 9;
+    int actual = calculator.divide(27, 3);
+    assertEquals(expected, actual);
+  }
+
 }
